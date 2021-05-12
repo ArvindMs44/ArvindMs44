295e3a0bdcfc620f1a441ce1a11590e939aaebcf
ArvindMs44 <44949702+ArvindMs44@users.noreply.github.com>
2021-05-12 19:55:12 +0530

    First Commit

---
+++ Test5.java
@@ -0,0 +1,37 @@
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+public class Test5 {
+
+		public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "D:\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
+		JavascriptExecutor JS = (JavascriptExecutor)driver;
+		JS.executeScript("window.scrollBy(0,500)");
+		JS.executeScript("document.querySelector('.tableFixHead').scrollTop=5000");
+		List<WebElement> Values_Locators = driver.findElements(By.xpath("//table[@id='product']//td[4]"));
+		 int sum = 0;
+		 for (int i = 0; i < Values_Locators.size(); i++) {
+			
+			  int Integer_Values = Integer.parseInt(Values_Locators.get(i).getText());
+			 sum+=Integer_Values;
+			  
+		}
+			System.out.println(sum);
+			String text = driver.findElement(By.xpath("//div[contains(text(),'Total Amount Collected: 296')]")).getText().split(":")[1].trim();
+			int Expected = Integer.parseInt(text);
+			Assert.assertEquals(Expected,sum);
+		driver.close();
+	}
+
+}
